a9238049d89afb04820be41b72fdd21a2b0be71c
alhamzah <alhamzah.alnufaili@gmail.com>
2020-06-07 23:05:24 -0400

    fixed issues with vundle, commands

--- setup_vm.sh
+++ setup_vm.sh
@@ -2,9 +2,8 @@
 
 git clone --separate-git-dir=$HOME/.myconf https://github.com/alhamzah/config.git $HOME/myconf-tmp
 rm -r ~/myconf-tmp/
-/usr/bin/git --git-dir=$HOME/.myconf/ status.showUntrackedFiles no
+/usr/bin/git --git-dir=$HOME/.myconf/ config status.showUntrackedFiles no
 /usr/bin/git --git-dir=$HOME/.myconf/ --work-tree=$HOME checkout .
 
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
-
-source $HOME/.bashrc
